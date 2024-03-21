c2e8afcc7919ea02dbaf7200e79483b32dadf445
Nathan Bak <apollo@yellowslicker.com>
2024-03-20 19:08:27 -0700

    Add query params

--- internal/server/api_memes.go
+++ internal/server/api_memes.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/NathanBak/maas/pkg/meme"
 )
@@ -9,10 +12,49 @@ import (
 func (s *Server) getMeme(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
+	latVal := r.URL.Query().Get("lat")
+	lonVal := r.URL.Query().Get("lon")
+	query := r.URL.Query().Get("query")
+
+	var lat float64
+	if latVal != "" {
+		parsed, err := strconv.ParseFloat(latVal, 64)
+		if err != nil {
+			s.RespondWithError(r.Context(), w, r, http.StatusBadRequest,
+				MAAS1000001, "invalid lattitude", nil)
+			return
+		}
+		// TODO:  Verify latitude is valid
+		lat = parsed
+	}
+
+	var lon float64
+	if latVal != "" {
+		parsed, err := strconv.ParseFloat(lonVal, 64)
+		if err != nil {
+			s.RespondWithError(r.Context(), w, r, http.StatusBadRequest,
+				MAAS1000002, "invalid longitude", nil)
+			return
+		}
+		// TODO:  Verify longitude is valid
+		lon = parsed
+	}
+
+	if query != "" {
+		// TODO: Add checking for injection attacks
+		if strings.TrimSpace(query) == "" {
+			s.RespondWithError(r.Context(), w, r, http.StatusBadRequest,
+				MAAS1000003, "invalid query", nil)
+			return
+		}
+	}
+
+	s.log.Debug(fmt.Sprintf("Lon: %f, Lat: %f, Query: %s", lon, lat, query))
+
 	meme, err := meme.NewTextMeme("All your base are belong to us")
 	if err != nil {
 		s.RespondWithError(r.Context(), w, r, http.StatusInternalServerError,
-			MAAS1000001, "unable to retrieve meme", nil)
+			MAAS1000004, "unable to retrieve meme", nil)
 		return
 	}
 
